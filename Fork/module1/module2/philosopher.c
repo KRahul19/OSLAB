1d8b0764906086d08811918d469d1268c13eaff9
KRahul19 <124139730+KRahul19@users.noreply.github.com>
2023-07-25 09:20:31 +0000

    commit

---
+++ Fork/module1/module2/philosopher.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<semaphore.h>
+#include<unistd.h>
+
+sem_t room;           // counting semaphore
+sem_t chopstick[5];   // binary semaphore
+
+void * philosopher(void *);
+void eat(int);
+
+void eat(int phil)
+{
+	printf("\nPhilosopher %d is eating",phil);
+}
+
+int main()
+{
+	int i,a[5];
+	pthread_t tid[5];       // creation of threads refering to 5 philosophers
+	
+	sem_init(&room,0,4);   // initializations of semaphore  varring from 0 to 4.
+	
+	for(i=0;i<5;i++)        
+		sem_init(&chopstick[i],0,1);  //initializations of binary semaphore .   
+		
+	for(i=0;i<5;i++){
+		a[i]=i;
+		pthread_create(&tid[i],NULL,philosopher,(void *)&a[i]);  // creation of philosopher and assigning it a number.
+	}
+	for(i=0;i<5;i++)
+		pthread_join(tid[i],NULL);  // waits until a thread gets terminated
+}
+
+void * philosopher(void * num)
+{
+	int phil=*(int *)num;
+
+	sem_wait(&room);                         // semaphore function to checks if resources are available.
+	printf("\nPhilosopher %d has entered room",phil);
+	sem_wait(&chopstick[phil]);              // semaphore function to checks if chopstick is available.
+	sem_wait(&chopstick[(phil+1)%5]);
+
+	eat(phil);
+	sleep(2);
+	printf("\nPhilosopher %d has finished eating",phil);
+
+	sem_post(&chopstick[(phil+1)%5]);   // gives confirmation if semophore is released successfully 
+	sem_post(&chopstick[phil]);
+	sem_post(&room);
+}
+
+
+// Philosopher 2 has entered room
+// Philosopher 2 is eating
+// Philosopher 1 has entered room
+// Philosopher 0 has entered room
+// Philosopher 3 has entered room
+// Philosopher 2 has finished eating
+// Philosopher 3 is eating
+// Philosopher 1 is eating
+// Philosopher 4 has entered room
+// Philosopher 3 has finished eating
+// Philosopher 1 has finished eating
+// Philosopher 0 is eating
+// Philosopher 0 has finished eating
+// Philosopher 4 is eating
